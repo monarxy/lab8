edf84b33d31846767534a3e065c6de170d9f95b5
иванов михаил <michael14183@gmail.com>
2023-09-30 08:11:04 +0300

    jgjgj

--- time.pas
+++ time.pas
@@ -1,5 +1,5 @@
 ﻿begin
   var c:= readinteger;
-  if (c mod 4 = 0) and (c mod 100 <> 0) and (c mod 400 <> 0) then print('true')
-  else print('false');
+  if (c mod 4 = 0) and (c mod 100 <> 0) and (c mod 400 <> 0) then print('366')
+  else print('365');
 end.
