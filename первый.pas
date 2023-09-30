158a6cea46132bf92b753076ca53f91a5803aff2
иванов михаил <michael14183@gmail.com>
2023-09-30 07:58:06 +0300

    ghbdtn

--- первый.pas
+++ первый.pas
@@ -1,3 +1,8 @@
 ﻿begin
-  print('hello world');
+ var a:= readinteger;
+ case a of 
+   4..10: print('доброе утро');
+   11..16: print('добрый день');
+   17..22: print('добрый вечер');
+ end;
 end.
